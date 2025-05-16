771d3103ec6f5c4bb0f036278950aa18b5340e9c
Vitor Paz <Kngatuno@gmail.com>
2025-05-16 12:27:02 -0300

    Enviando alterações feitas no notebook

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.http import HttpResponse
+from recipes.views import home, contato, sobre
+    
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
 ]
